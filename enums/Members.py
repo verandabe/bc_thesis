158c083a890f6ab3cfbbc876d88904fceedb44dd
Veronika <burgerova-veronika@email.cz>
2022-02-08 13:06:33 +0100

    Enum members update

--- enums/Members.py
+++ enums/Members.py
@@ -1,15 +1,24 @@
 from enum import Enum
 
 class Member(Enum):
+
+    # super members
     sentence = '<sentence>'
     clause = '<clause>'
+    coord = '<coord>'
 
-    verb = 0
+    # not as described members
+    verb = 'verb'
+    other = 'other'
 
+    # as described members
     auxiliary_verb = 'auxiliary-verb'
     subject = 'subject'
+    subject_bad = 'subject-bad'
     object = 'object'
     verb_object = 'verb-object'
     modifier = 'modifier'
     adverb = 'adverb'
+    additional_prep = 'additional-prep'
+    prep_object = 'prep-object'
 
