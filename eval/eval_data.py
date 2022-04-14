6feb850a1e64cef2598fdd6cf280fad9484d6d22
Veronika Burgerova <xburger@aurora.fi.muni.cz>
2022-04-14 16:18:15 +0200

    fix eval data

--- eval/eval_data.py
+++ eval/eval_data.py
@@ -1,3 +1,11 @@
+#import sys
+#sys.path.append('./enums/')
+#sys.path.append('./classes/')
+#sys.path.append('./tools/')
+#sys.path.append('./')
+#sys.path.append('./')
+
+
 import os
 from RephrasErIch import RephrasErIch
 from Forms import Form
@@ -21,16 +29,19 @@ def read_text_file(filename):
 
 def create_pairs():
     for filename in os.listdir(datadir):
-        form, protg, text = read_text_file(filename)
+        if os.path.isfile(datadir+filename):
+            form, protg, text = read_text_file(filename)
 
-
-        rerich = RephrasErIch(Form[form])
-        rerich.create_protagonist(protg)
-        rephrased_text = rerich.rephrase(text)
-
-        with open(datadir+'rephrased_' + filename, 'w') as rf:
-            rf.write("Metadata: from_form=" + form + ", protagonist=" + protg + '\n')
-            rf.write("Original: " + text)
-            rf.write("\n")
-            rf.write("Rephrased: " + rephrased_text)
-
+            print('form:', form)
+            print('object form:', Form[form])
+            
+            rerich = RephrasErIch(Form[form])
+            rerich.create_protagonist(protg)
+            rephrased_text = rerich.rephrase(text)
+            
+            with open(datadir+'rephrased/rephrased_' + filename, 'w') as rf:
+                rf.write("Metadata: from_form=" + form + ", protagonist=" + protg + '\n')
+                rf.write("Original: " + text)
+                rf.write("\n")
+                rf.write("Rephrased: " + rephrased_text)
+            
