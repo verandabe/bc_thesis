5d2bc00c7eb9257524cf75a94ce9a43905baa671
Veronika Burgerova <xburger@aurora.fi.muni.cz>
2022-05-09 13:22:17 +0200

    simplify main

--- main.py
+++ main.py
@@ -4,18 +4,9 @@ sys.path.append('./enums/')
 sys.path.append('./classes/')
 sys.path.append('./tools/')
 sys.path.append('./')
-sys.path.append('./eval/')
 
 from RephrasErIch import RephrasErIch
 from Forms import Form
-from Word import Word
-from Protagonist import Protagonist
-from Members import Member
-from syn import Syn
-
-from eval_data import *
-
-#create_pairs()
 
 
 text = "Petr chce, aby máma plakala."
@@ -23,6 +14,5 @@ text = "Petr chce, aby máma plakala."
 rerich = RephrasErIch(Form.ER)
 rerich.create_protagonist("máma")
 print(rerich.rephrase(text))
-#'''
 
 
